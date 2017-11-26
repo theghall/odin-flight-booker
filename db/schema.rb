0c7c9157b8330ecf474da0b0c6ada06a97b0df28
Gary Hall <guategary@gmail.com>
2017-11-26 16:37:16 -0500

    Add Airport model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126210953) do
+ActiveRecord::Schema.define(version: 20171126213040) do
+
+  create_table "airports", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Passenger.create(name: 'Jazzy Jetsetter', email: 'jetsetter@vidaloco.com')
 Passenger.create(name: 'Ernie Economy', email: 'ernie@economy.com')
 Passenger.create(name: 'Fran Frequent', email: 'freddie@frequent.com')
 Passenger.create(name: 'Annie Airliner', email: 'annie@airliner.com')
+
+Airport.delete_all
+Airport.create(code: 'PHL')
+Airport.create(code: 'IAH')
+Airport.create(code: 'SLC')
+Airport.create(code: 'MCO')
+Airport.create(code: 'BDL')
+Airport.create(code: 'EWR')
